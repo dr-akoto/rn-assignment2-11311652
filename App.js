c2eb2aa26b9edc87ccf78b6f0c614bd24fb365e3
DR.AKOTO <149382286+dr-akoto@users.noreply.github.com>
2024-05-27 16:20:01 +0000

    Update App.js

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },

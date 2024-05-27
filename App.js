7e5a57a584ca5c39d492eb4a49c64a044c3c2c46
DR.AKOTO <149382286+dr-akoto@users.noreply.github.com>
2024-05-27 16:04:58 +0000

    Add files via upload
    created an expo app

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <Text style={styles.pen}>My name is <Text style={styles.pin}> NANA OSEI OWUSU AKOTO</Text></Text>
+    
+    <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pen: {
+      fontSize: 24,
+  },
+  pin: {
+      fontSize: 24,
+      fontWeight: 'bold',
+  },
+});
